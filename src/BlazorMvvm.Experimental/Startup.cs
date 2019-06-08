37a18c366947563329a333644306d830b14117cb
Håvard Moås <haavamoa@gmail.com>
2019-06-08 20:39:48 +0200

    Cleaned up and added CounterView with a CounterViewModel

--- src/BlazorMvvm.Experimental/Startup.cs
+++ src/BlazorMvvm.Experimental/Startup.cs
@@ -1,3 +1,4 @@
+using BlazorMvvm.Experimental.ViewModels;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,7 @@ namespace BlazorMvvm.Experimental
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<CounterViewModel>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
---
+++ src/BlazorMvvm.Experimental/ViewModels/CounterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+using BlazorMvvm.Experimental.Helpers;
+
+namespace BlazorMvvm.Experimental.ViewModels
+{
+    public class CounterViewModel
+    {
+        public CounterViewModel()
+        {
+            IncrementCounterCommand = new DelegateCommand(_ => Counter++);
+            DecrementCounterCommand = new DelegateCommand(_ => Counter--);
+        }
+
+        public int Counter { get; private set; }
+
+        public ICommand DecrementCounterCommand { get; set; }
+        public ICommand IncrementCounterCommand { get; }
+    }
+}
